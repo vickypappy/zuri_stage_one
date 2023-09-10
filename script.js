497b64007aff8ef9a77a841423f1a7f150a6c447
Victor <vickypappy01@gmail.com>
2023-09-10 23:00:32 +0100

    Feat: added another github repo

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-// Function to display UTC time in milliseconds
 function displayUTCTimeMilliseconds() {
   const utcTimeMillisecondsElement = document.querySelector('[data-testid="currentUTCTime"');
   
   setInterval(() => {
       const currentTimeMillis = new Date().getTime();
-      utcTimeMillisecondsElement.textContent = `UTC Time: ${currentTimeMillis}`;
+      utcTimeMillisecondsElement.textContent = `${currentTimeMillis}`;
   }, 1000);
 }
-
-// Function to display the day of the week
 function displayDayOfWeek() {
   const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
   
   setInterval(() => {
       const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
       const currentDayOfWeek = new Date().getDay();
-      dayOfWeekElement.textContent = `Day of the Week: ${days[currentDayOfWeek]}`;
+      dayOfWeekElement.textContent = `${days[currentDayOfWeek]}`;
   }, 1000);
 }
 
